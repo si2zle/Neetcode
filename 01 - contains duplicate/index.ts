4d7d2c5ce21ea54b6273fe44c1dd1b0ccae35e54
Jace <jaysondaji@gmail.com>
2023-09-30 16:26:20 +1300

    First problem in the Neetcode challenges

---
+++ 01 - contains duplicate/index.ts
@@ -0,0 +1,20 @@
+function containsDuplicate(nums: number[]): boolean {
+    // create a set from nums
+    const set = new Set(nums);
+    // check if the set size is the same as the nums length, if it is, then there are no duplicates
+    return set.size !== nums.length;
+}
+
+function containsDuplicate_two(nums: number[]): boolean {
+    // create a new set
+    const set = new Set();
+    for (let num of nums) {
+        // check if the current num is in the set
+        if (set.has(num)) {
+            return true;
+        }
+        // add the current num to the set
+        set.add(num);
+    }
+    return false;
+}
